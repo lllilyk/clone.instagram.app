95316e5797ae243f659a503952d7c6a7aa864e57
lllilyk <ym0402kkk@naver.com>
2024-03-15 22:54:30 +0900

    users 패키지 - views.py 설정py

--- service/users/views.py
+++ service/users/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+# django modules
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
 
-# Create your views here.
+# drf modules
+from rest_framework import status
+from rest_framework import Response
+from rest_framework import ModelViewSets
+from rest_framework.permissions import AllowAny
+
+# modules
+from users.models import User
+
+# serializers
+from users.serializers import UserSerializer
+
+class AuthViewSet(ModelViewSets):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permissions = [AllowAny]
+
+    def signup(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid()
+        user = serializer.save()
+        login(request, user)
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED
+        ) 
