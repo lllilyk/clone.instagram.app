e34ee14ce2f41b7c8a04f01784eb1abdde977bc8
lllilyk <ym0402kkk@naver.com>
2024-03-15 23:48:43 +0900

    ModuleNotFoundError - 모델을 임포트 하는 부분을 수정하여 모델을 올바르게 참조하도록 함

--- service/users/serializers.py
+++ service/users/serializers.py
@@ -1,7 +1,7 @@
 # model serializer 생성
 from rest_framework import serializers
 
-from models import User
+from users.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
